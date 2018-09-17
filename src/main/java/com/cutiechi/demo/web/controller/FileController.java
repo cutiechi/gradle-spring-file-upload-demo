20bc012e87715f4bb289b36bb12186e7618e08c5
cutiechi <chi@cutie.ooo>
2018-09-17 20:25:28 +0800

    feat(controller): add file rest controller

---
+++ src/main/java/com/cutiechi/demo/web/controller/FileController.java
@@ -0,0 +1,15 @@
+package com.cutiechi.demo.web.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 文件 REST 控制器
+ *
+ * @author Cutie Chi
+ */
+@RestController
+@RequestMapping("/files")
+public class FileController {
+
+}
