fb8668132848415bf442ba5fbe0ae65cece61b02
cutiechi <chi@cutie.ooo>
2018-09-18 13:47:05 +0800

    feat(service): add file service interface and impl

---
+++ src/main/java/com/cutiechi/demo/service/FileService.java
@@ -0,0 +1,10 @@
+package com.cutiechi.demo.service;
+
+/**
+ * 文件业务逻辑层接口
+ *
+ * @author Cutie Chi
+ */
+public interface FileService {
+
+}
---
+++ src/main/java/com/cutiechi/demo/service/impl/FileServiceImpl.java
@@ -0,0 +1,15 @@
+package com.cutiechi.demo.service.impl;
+
+import com.cutiechi.demo.service.FileService;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * 文件业务逻辑层实现类
+ *
+ * @author Cutie Chi
+ */
+@Service
+public class FileServiceImpl implements FileService {
+
+}
