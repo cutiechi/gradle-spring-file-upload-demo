22f99220354f518ce452e22a25b4047b74731f0a
cutiechi <chi@cutie.ooo>
2018-09-18 14:00:24 +0800

    feat(handler): add global exception handler

---
+++ src/main/java/com/cutiechi/demo/handler/GlobalExceptionHandler.java
@@ -0,0 +1,13 @@
+package com.cutiechi.demo.handler;
+
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+/**
+ * 全局异常处理类
+ *
+ * @author Cutie Chi
+ */
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+}
