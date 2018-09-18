433dee0774b22cd3b2ff19c570086b4c5e9d42b4
cutiechi <chi@cutie.ooo>
2018-09-18 13:52:47 +0800

    feat(exception): add file upload exception

---
+++ src/main/java/com/cutiechi/demo/exception/FileUploadException.java
@@ -0,0 +1,13 @@
+package com.cutiechi.demo.exception;
+
+/**
+ * 文件上传异常
+ *
+ * @author Cutie Chi
+ */
+public class FileUploadException extends Exception {
+
+    public FileUploadException (String message) {
+        super(message);
+    }
+}
