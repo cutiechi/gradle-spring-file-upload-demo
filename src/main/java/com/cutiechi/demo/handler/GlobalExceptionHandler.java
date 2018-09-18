0be4cb7ca5e3f866f358137ef31fb06fc30c3aff
cutiechi <chi@cutie.ooo>
2018-09-18 14:10:40 +0800

    feat(handler): add file upload exception handle method

--- src/main/java/com/cutiechi/demo/handler/GlobalExceptionHandler.java
+++ src/main/java/com/cutiechi/demo/handler/GlobalExceptionHandler.java
@@ -1,6 +1,13 @@
 package com.cutiechi.demo.handler;
 
+import com.cutiechi.demo.exception.FileUploadException;
+import com.cutiechi.demo.model.dto.JsonResponse;
+
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import static com.cutiechi.demo.model.dto.JsonResponse.Status.INTERNAL_SERVER_ERROR;
 
 /**
  * 全局异常处理类
@@ -10,4 +17,16 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
+
+    /**
+     * 文件上传异常处理方法
+     *
+     * @param exception 文件上传异常
+     * @return 通用 JSON 响应
+     */
+    @ExceptionHandler(FileUploadException.class)
+    @ResponseBody
+    public JsonResponse fileUploadException (FileUploadException exception) {
+        return new JsonResponse(INTERNAL_SERVER_ERROR.getValue(), exception.getMessage());
+    }
 }
