7245ced93f05d245640fd6f4b067b712b2dc8b8d
cutiechi <chi@cutie.ooo>
2018-09-18 13:58:03 +0800

    feat(service): add upload file method

--- src/main/java/com/cutiechi/demo/service/FileService.java
+++ src/main/java/com/cutiechi/demo/service/FileService.java
@@ -1,5 +1,10 @@
 package com.cutiechi.demo.service;
 
+import com.cutiechi.demo.exception.FileUploadException;
+import com.cutiechi.demo.model.dto.ServiceResult;
+
+import org.springframework.web.multipart.MultipartFile;
+
 /**
  * 文件业务逻辑层接口
  *
@@ -7,4 +12,12 @@ package com.cutiechi.demo.service;
  */
 public interface FileService {
 
+    /**
+     * 上传文件
+     *
+     * @param files 文件数组
+     * @return 业务逻辑结果
+     * @throws FileUploadException 文件上传异常
+     */
+    ServiceResult upload (MultipartFile[] files) throws FileUploadException;
 }
--- src/main/java/com/cutiechi/demo/service/impl/FileServiceImpl.java
+++ src/main/java/com/cutiechi/demo/service/impl/FileServiceImpl.java
@@ -1,8 +1,14 @@
 package com.cutiechi.demo.service.impl;
 
+import com.cutiechi.demo.exception.FileUploadException;
+import com.cutiechi.demo.model.dto.ServiceResult;
 import com.cutiechi.demo.service.FileService;
 
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
 
 /**
  * 文件业务逻辑层实现类
@@ -12,4 +18,28 @@ import org.springframework.stereotype.Service;
 @Service
 public class FileServiceImpl implements FileService {
 
+    /**
+     * 上传文件
+     *
+     * @param files 文件数组
+     * @return 业务逻辑结果
+     * @throws FileUploadException 文件上传异常
+     */
+    @Override
+    public ServiceResult upload (MultipartFile[] files) throws FileUploadException {
+        final String FILES_DICTIONARY = "D:\\Codes\\Spring\\gradle-spring-file-upload-demo\\";
+        try {
+            if (files.length > 0) {
+                for (MultipartFile file : files) {
+                    String fileName = file.getOriginalFilename();
+                    file.transferTo(new File(FILES_DICTIONARY + fileName));
+                }
+                return ServiceResult.success("上传文件成功！");
+            } else {
+                return ServiceResult.fail("没有选择任何文件，上传文件失败！");
+            }
+        } catch (IOException exception) {
+            throw new FileUploadException("服务器错误，上传文件失败！");
+        }
+    }
 }
